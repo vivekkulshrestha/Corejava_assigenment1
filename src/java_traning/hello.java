8ea4b92fc09b6f182cba68b71facc7511ee53eeb
vivek <Vivek@VivekKulshrestha>
2017-09-16 10:47:55 +0530

    Assigenment 1.1

---
+++ src/java_traning/hello.java
@@ -0,0 +1,12 @@
+package java_traning;
+
+public class hello {
+
+	public static void main(String[] args) 
+	{
+		// Printing the Hello World in Output
+		System.out.println("Hello World");
+
+	}
+
+}
